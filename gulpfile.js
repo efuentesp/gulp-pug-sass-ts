bd372f548dea68ba2cdba452fa08fb1f584eead0
Edgar Felipe Fuentes Perea (EFP) <efuentesp@gmail.com>
2019-08-10 10:26:19 -0500

    Pantallas funcionando

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ const gulp = require("gulp");
 const pug = require("gulp-pug");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
+const replace = require("gulp-replace");
+const sourcemaps = require("gulp-sourcemaps");
 const typescript = require("gulp-typescript");
 const browserSync = require("browser-sync").create();
+const lec = require("gulp-line-ending-corrector");
 
 // function clean() {
 //   return gulp
@@ -12,26 +15,35 @@ const browserSync = require("browser-sync").create();
 // }
 
 function pugIt() {
-  return gulp
-    .src("./src/views/pages/**/*.pug")
-    .pipe(
-      pug({
-        pretty: true
-      })
-    )
-    .pipe(gulp.dest("./dist"))
-    .pipe(browserSync.stream());
+  return (
+    gulp
+      .src("./src/views/pages/**/*.pug")
+      .pipe(
+        pug({
+          pretty: true
+        })
+      )
+      // .pipe(replace(/(\.)pug/g, ".html"))
+      // .pipe(replace(/(\.)scss/g, ".css"))
+      // .pipe(replace(/(\.)ts/g, ".js"))
+      .pipe(lec())
+      .pipe(gulp.dest("./dist"))
+      .pipe(browserSync.stream())
+  );
 }
 
 function sassIt() {
   return gulp
     .src(["./src/views/styles/main.scss", "./src/views/pages/**/*.scss"])
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(sass().on("error", sass.logError))
     .pipe(
       autoprefixer({
         overrideBrowserslist: ["last 2 versions"]
       })
     )
+    .pipe(sourcemaps.write())
+    .pipe(lec())
     .pipe(gulp.dest("./dist/assets/css"))
     .pipe(browserSync.stream());
 }
@@ -45,6 +57,7 @@ function typescriptIt() {
         module: "none"
       })
     )
+    .pipe(lec())
     .pipe(gulp.dest("./dist/assets/scripts"))
     .pipe(browserSync.stream());
 }
@@ -58,13 +71,18 @@ function watch() {
 
   gulp.watch(
     ["./src/views/styles/main.scss", "./src/views/pages/**/*.scss"],
-    gulp.series(["sassIt"])
+    sassIt
   );
-  gulp.watch("./**/*.pug", gulp.series(["pugIt"]));
-  gulp.watch("./ts/**/*.ts", gulp.series(["typescriptIt"]));
+  gulp.watch("./**/*.pug", pugIt);
+  gulp.watch("./ts/**/*.ts", typescriptIt);
 }
 
 exports.sassIt = sassIt;
 exports.pugIt = pugIt;
 exports.typescriptIt = typescriptIt;
 exports.watch = watch;
+
+exports.default = gulp.series(
+  gulp.parallel(pugIt, sassIt, typescriptIt),
+  watch
+);
