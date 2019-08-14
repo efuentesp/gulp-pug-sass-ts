8cd76ac73fe52a4978a09ffa33dd9e93f94ac306
Edgar Felipe Fuentes Perea (EFP) <efuentes@softtek.com>
2019-08-14 18:39:13 -0500

    Se agrega TSLint.

--- src/views/pages/accimarket/01-caucion-bursatil.ts
+++ src/views/pages/accimarket/01-caucion-bursatil.ts
@@ -1,103 +1,103 @@
-console.log("01-caucion-bursatil");
+console.log('01-caucion-bursatil');
 
-$("#criterios_busqueda_accordion").accordion(ui_accordion_settings);
+$('#criterios_busqueda_accordion').accordion(ui_accordion_settings);
 
-$("#fecha").datepicker(ui_datepicker_settings);
+$('#fecha').datepicker(ui_datepicker_settings);
 
 const rest_url = `${REST_URL}/fideicomiso`;
 
-$("#table_contratos").jqGrid({
-  datatype: "local",
-  height: 250,
-  colNames: [
-    "Contrato",
-    "Libro",
-    "Cliente",
-    "Digito",
-    "TV",
-    "Descr TV",
-    "Emisora",
-    "Serie",
-    "Cantidad",
-    "Precio",
-    "Valuación",
-    "Promotor",
-    "Folio",
-    "Fecha",
-    "Negocio"
-  ],
-  colModel: [
-    { name: "contrato", width: 55 },
-    { name: "libro", width: 90 },
-    { name: "cliente", width: 80, align: "right" },
-    { name: "digito", width: 80, align: "right" },
-    { name: "tv", width: 80, align: "right" },
-    { name: "descr_tv", width: 150, sortable: false },
-    { name: "emisora", width: 90 },
-    { name: "serie", width: 90 },
-    { name: "cantidad", width: 90 },
-    { name: "precio", width: 90 },
-    { name: "valuacion", width: 90 },
-    { name: "promotor", width: 90 },
-    { name: "folio", width: 90 },
-    { name: "fecha", width: 90 },
-    { name: "negocio", width: 90 }
-  ],
-  pager: "#pager_contratos",
-  rowNum: 10,
-  rowList: [10, 20, 30],
-  sortname: "contrato",
-  sortorder: "desc",
-  viewrecords: true,
-  gridview: true,
-  autoencode: true,
-  caption: ""
+$('#table_contratos').jqGrid({
+	datatype: 'local',
+	height: 250,
+	colNames: [
+		'Contrato',
+		'Libro',
+		'Cliente',
+		'Digito',
+		'TV',
+		'Descr TV',
+		'Emisora',
+		'Serie',
+		'Cantidad',
+		'Precio',
+		'Valuación',
+		'Promotor',
+		'Folio',
+		'Fecha',
+		'Negocio'
+	],
+	colModel: [
+		{ name: 'contrato', width: 55 },
+		{ name: 'libro', width: 90 },
+		{ name: 'cliente', width: 80, align: 'right' },
+		{ name: 'digito', width: 80, align: 'right' },
+		{ name: 'tv', width: 80, align: 'right' },
+		{ name: 'descr_tv', width: 150, sortable: false },
+		{ name: 'emisora', width: 90 },
+		{ name: 'serie', width: 90 },
+		{ name: 'cantidad', width: 90 },
+		{ name: 'precio', width: 90 },
+		{ name: 'valuacion', width: 90 },
+		{ name: 'promotor', width: 90 },
+		{ name: 'folio', width: 90 },
+		{ name: 'fecha', width: 90 },
+		{ name: 'negocio', width: 90 }
+	],
+	pager: '#pager_contratos',
+	rowNum: 10,
+	rowList: [10, 20, 30],
+	sortname: 'contrato',
+	sortorder: 'desc',
+	viewrecords: true,
+	gridview: true,
+	autoencode: true,
+	caption: ''
 });
 
 // Form validations
-let contratos_params : UrlParams = {
-  // cliente: "CLIENTE 01",
-  // contrato: "CONTRATO 01"
+let contratos_params: UrlParams = {
+	// cliente: "CLIENTE 01",
+	// contrato: "CONTRATO 01"
 };
 
-http_findAll("contratos", contratos_params, payload => {
-  fillJqGrid("#table_contratos", payload)
+http_findAll('contratos', contratos_params, payload => {
+	fillJqGrid('#table_contratos', payload);
 });
 
-const form = ($("#criterios-busqueda") as any)
-  .parsley()
-  .on("field:validated", () => {
-    const ok = $(".parsley-error").length === 0;
-    // console.log("Errores de validación", $(".parsley-error"))
-    // $(".callout-info").toggleClass("hidden", !ok);
-    // $(".callout-warning").toggleClass("hidden", ok);
-  })
-  .on("form:submit", () => {
-    console.log("form:submit");
+const form = ($('#criterios-busqueda') as any)
+	.parsley()
+	.on('field:validated', () => {
+		const ok = $('.parsley-error').length === 0;
+		// console.log("Errores de validación", $(".parsley-error"))
+		// $(".callout-info").toggleClass("hidden", !ok);
+		// $(".callout-warning").toggleClass("hidden", ok);
+	})
+	.on('form:submit', () => {
+		console.log('form:submit');
 
-    contratos_params = {};
+		contratos_params = {};
 
-    const fecha = $("#fecha").val();
-    const contrato = $("#contrato").val();
-    const digito = $("#digito").val();
-    const negocio = $("input[name='negocio']:checked").val();
+		const fecha = $('#fecha').val();
+		const contrato = $('#contrato').val();
+		const digito = $('#digito').val();
+		const negocio = $("input[name='negocio']:checked").val();
 
-    if(fecha) {
-      contratos_params.fecha = fecha
-    }
-    if(contrato) {
-      contratos_params.contrato = contrato;
-    }
-    if(digito) {
-      contratos_params.digito = digito;
-    }
-    if(negocio) {
-      contratos_params.negocio = negocio
-    }
+		if (fecha) {
+			contratos_params.fecha = fecha;
+		}
+		if (contrato) {
+			contratos_params.contrato = contrato;
+		}
+		if (digito) {
+			contratos_params.digito = digito;
+		}
+		if (negocio) {
+			contratos_params.negocio = negocio;
+		}
 
-    http_findAll("contratos", contratos_params, payload => {
-      fillJqGrid("#table_contratos", payload)
-    });
+		http_findAll('contratos', contratos_params, payload => {
+			fillJqGrid('#table_contratos', payload);
+		});
 
-    return false;
-  });
+		return false;
+	});
