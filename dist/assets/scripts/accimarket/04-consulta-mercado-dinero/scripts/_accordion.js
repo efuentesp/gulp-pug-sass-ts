b753660560a73f4451e3743b21641c6173d3c785
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2019-08-18 20:26:28 -0500

    refactor de pantallas en folders.

--- dist/assets/scripts/accimarket/04-consulta-mercado-dinero/scripts/_accordion.js
+++
@@ -1,3 +0,0 @@
-$('#criterios_busqueda_accordion').accordion(ui_accordion_settings);
-
-//# sourceMappingURL=../../../../maps/accimarket/04-consulta-mercado-dinero/scripts/_accordion.js.map
---
+++ dist/assets/scripts/accimarket/form-validation/form-validation.js
@@ -0,0 +1,14 @@
+$("#new-customer")
+    .parsley()
+    .on("field:validated", function () {
+    var ok = $(".parsley-error").length === 0;
+    $(".callout-info").toggleClass("hidden", !ok);
+    $(".callout-warning").toggleClass("hidden", ok);
+})
+    .on("form:submit", function () {
+    console.log("form:submit");
+    alert("Form ready to be sent!");
+    return false;
+});
+
+//# sourceMappingURL=../../../maps/accimarket/form-validation/form-validation.js.map
