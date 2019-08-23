a61d3ffe4920f0393e858f33f409b5254e0eb4d1
Edgar Felipe Fuentes Perea (EFP) <efuentes@softtek.com>
2019-08-23 17:08:22 -0500

    Se arreglan los campos obligatorios.

--- src/views/pages/accimarket/form-validation/scripts.ts
+++ src/views/pages/accimarket/form-validation/scripts.ts
@@ -1,12 +1,24 @@
-($("#new-customer") as any)
-  .parsley()
-  .on("field:validated", () => {
-    const ok = $(".parsley-error").length === 0;
-    $(".callout-info").toggleClass("hidden", !ok);
-    $(".callout-warning").toggleClass("hidden", ok);
-  })
-  .on("form:submit", () => {
-    console.log("form:submit");
-    alert("Form ready to be sent!");
-    return false;
-  });
+($('#new-customer') as any)
+	.parsley()
+	.on('field:validated', () => {
+		const ok = $('.parsley-error').length === 0;
+		$('.callout-info').toggleClass('hidden', !ok);
+		$('.callout-warning').toggleClass('hidden', ok);
+	})
+	.on('form:submit', () => {
+		console.log('form:submit');
+		alert('Form ready to be sent!');
+		return false;
+	});
+
+($('#payment') as any).select2({
+	minimumResultsForSearch: Infinity
+});
+
+($('#payment2') as any).select2({
+	minimumResultsForSearch: Infinity
+});
+
+($('#payment3') as any).select2({
+	minimumResultsForSearch: Infinity
+});
