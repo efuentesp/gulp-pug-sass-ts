0b881a4641ff9547ecbea5f43aaf4c5c051b3cbb
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2019-08-25 19:07:31 -0500

    Se agregan validaciones al quiz

--- src/views/pages/accimarket/form-validation/scripts.ts
+++ src/views/pages/accimarket/form-validation/scripts.ts
@@ -1,24 +1,35 @@
-($('#new-customer') as any)
-	.parsley()
-	.on('field:validated', () => {
-		const ok = $('.parsley-error').length === 0;
-		$('.callout-info').toggleClass('hidden', !ok);
-		$('.callout-warning').toggleClass('hidden', ok);
-	})
-	.on('form:submit', () => {
-		console.log('form:submit');
-		alert('Form ready to be sent!');
-		return false;
-	});
+($("#new-customer") as any)
+  .parsley()
+  .on("field:validated", () => {
+    const ok = $(".parsley-error").length === 0;
+    $(".callout-info").toggleClass("hidden", !ok);
+    $(".callout-warning").toggleClass("hidden", ok);
+  })
+  .on("form:submit", () => {
+    console.log("form:submit");
+    alert("Form ready to be sent!");
+    return false;
+  });
 
-($('#payment') as any).select2({
-	minimumResultsForSearch: Infinity
+($("#payment") as any).select2({
+  placeholder: "--Seleccione--",
+  minimumResultsForSearch: Infinity
 });
 
-($('#payment2') as any).select2({
-	minimumResultsForSearch: Infinity
+($("#payment2") as any).select2({
+  placeholder: "--Seleccione--",
+  minimumResultsForSearch: Infinity
 });
 
-($('#payment3') as any).select2({
-	minimumResultsForSearch: Infinity
+$("#payment2").change(() => {
+  ($("#new-customer") as any).parsley().validate();
+});
+
+($("#payment3") as any).select2({
+  placeholder: "--Seleccione--",
+  minimumResultsForSearch: Infinity
+});
+
+$("#payment3").change(() => {
+  ($("#new-customer") as any).parsley().validate();
 });
