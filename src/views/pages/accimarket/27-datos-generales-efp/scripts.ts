ff278320958527f9ac103d3ccae0ebfb8835ee95
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2019-08-27 17:40:09 -0500

    Se agrega la pantalla 27 Datos Personales

---
+++ src/views/pages/accimarket/27-datos-generales-efp/scripts.ts
@@ -0,0 +1,12 @@
+console.log("27-datos-generales-efp");
+
+$("#criterios_busqueda_accordion").accordion(ui_accordion_settings);
+
+$("#titular_cotitular_tabgroup").tabs();
+
+$("#titular_contrato_tabgroup")
+  .tabs()
+  .addClass("ui-tabs-vertical ui-helper-clearfix");
+$("#titular_contrato_tabgroup li")
+  .removeClass("ui-corner-top")
+  .addClass("ui-corner-left");
