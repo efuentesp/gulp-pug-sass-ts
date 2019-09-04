2460882f0373994ef6d6182a0b575caf3058c874
Luis Alberto Dominguez Palomino <luis.dominguezp@stkbanamex.com>
2019-09-04 13:20:08 -0500

    Cambios realizados por Luis Puentes

---
+++ src/views/pages/accimarket/30-reperfilamiento-persona-moral/scripts.ts
@@ -0,0 +1,33 @@
+$("#reperfilamiento-pm").accordion(ui_accordion_settings);
+
+$("#reperfilamiento_pm_tab_group").tabs();
+
+($("#antiguedadConsPM") as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+});
+
+($("#giroPM") as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+});
+
+($("#tipoPM") as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+});
+
+($("#actividadActuaPM") as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+});
+
+($("#estrategiaInvPM") as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+});
+
+($("#asesoriaInvPM") as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+});
