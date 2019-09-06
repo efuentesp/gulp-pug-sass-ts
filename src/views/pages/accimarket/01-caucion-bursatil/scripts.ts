2faf1ed45f37fcb913a7dbaddebcd0b1cfa8cb9b
Normaysel Carbajal <normaysel.carbajal@softtek.com>
2019-09-05 21:54:43 -0500

    Se agregan dos componentes de select plus minus

--- src/views/pages/accimarket/01-caucion-bursatil/scripts.ts
+++ src/views/pages/accimarket/01-caucion-bursatil/scripts.ts
@@ -8,16 +8,145 @@ $("#fecha").datepicker(ui_datepicker_settings);
 
 const rest_url = `${REST_URL}/fideicomiso`;
 
-$("#btn_plus_contrato").click(() => {
-  const text_to_add = $("#contrato").val() as string;
-  console.log(text_to_add);
-  $("ul#tag_list_contrato").append($("<li>").text(text_to_add));
-  $("#contrato").val("");
-});
+const fieldSelectPlusMinus = (id: string) => {
+  const idBtnPlus = "#btn_plus_" + id;
+  const idBtnMinus = "#btn_minus_" + id;
+  const idInput = "#" + id;
+  const list = "ul#tag_list_" + id;
 
-$("ul#tag_list_contrato li").click(() => {
-  console.log("li clicked!");
-});
+  $(idInput).append('<option value=""></option>');
+  $(idInput).append('<option value="AL1">Alabama 1</option>');
+  $(idInput).append('<option value="AL2">Alabama 2</option>');
+  $(idInput).append('<option value="AL3">Alabama 3</option>');
+  $(idInput).append('<option value="AL4">Alabama 4</option>');
+
+  $(idBtnPlus).click(() => {
+    const text_to_add = $("#test option:selected").text() as string;
+    const value_to_add = $("#test option:selected").val() as string;
+    var exist = 0;
+
+    if ($("li").length <= 0) {
+      $(list).append(
+        "<li><a id = " +
+          value_to_add +
+          " class='delete_item' href='javascript:void();'>" +
+          text_to_add +
+          "</option></a></li>"
+      );
+      exist = 1;
+    } else {
+      $(list + " li a").each(function(index) {
+        if ($(this).text() === text_to_add) {
+          exist = 1;
+          return false;
+        }
+      });
+    }
+
+    if (exist == 0) {
+      $(list).append(
+        "<li><a id = " +
+          value_to_add +
+          " class='delete_item' href='javascript:void();'>" +
+          text_to_add +
+          "</option></a></li>"
+      );
+    }
+
+    $(idInput)
+      .val(null)
+      .trigger("change");
+  });
+
+  $(idBtnMinus).click(() => {
+    $(list + " li a").each(function(index) {
+      if ($(this).attr("id") === $(idInput).val()) {
+        $(this).remove();
+      }
+    });
+
+    $(idInput)
+      .val(null)
+      .trigger("change");
+  });
+
+  $(list).delegate(".delete_item", "click", function() {
+    $(idInput)
+      .val(
+        $(this)
+          .parent()
+          .find(".delete_item")
+          .attr("id")
+      )
+      .trigger("change");
+  });
+
+  ($(idInput) as any).select2({
+    placeholder: "--Seleccione--",
+    minimumResultsForSearch: Infinity
+  });
+};
+
+const fieldPlusMinus = (id: string) => {
+  const idBtnPlus = "#btn_plus_" + id;
+  const idBtnMinus = "#btn_minus_" + id;
+  const idInput = "#" + id;
+  const list = "ul#tag_list_" + id;
+
+  $(idBtnPlus).click(() => {
+    const text_to_add = $(idInput).val() as string;
+    var exist = 0;
+
+    if ($("li").length <= 0) {
+      $(list).append(
+        '<li><a class="delete_item" href="javascript:void();">' +
+          text_to_add +
+          "</a> </li>"
+      );
+      exist = 1;
+    } else {
+      $(list + "li a").each(function(index) {
+        if ($(this).text() === text_to_add) {
+          exist = 1;
+          return false;
+        }
+      });
+    }
+
+    if (exist == 0) {
+      $(list).append(
+        '<li><a class="delete_item" href="javascript:void();">' +
+          text_to_add +
+          "</a> </li>"
+      );
+    }
+
+    $(idInput).val("");
+  });
+
+  $(idBtnMinus).click(() => {
+    $(list + " li a").each(function(index) {
+      if ($(this).text() === $(idInput).val()) {
+        $(this).remove();
+      }
+    });
+
+    $(idInput).val("");
+  });
+
+  $(list).delegate(".delete_item", "click", function() {
+    $(idInput).val(
+      $(this)
+        .parent()
+        .find(".delete_item")
+        .html()
+    );
+  });
+};
+
+fieldPlusMinus("contrato");
+fieldPlusMinus("digito");
+fieldSelectPlusMinus("test");
 
 $("#table_contratos").jqGrid({
   datatype: "local",
