9fe1b286ac849cac849e64906f593da350b76715
Edgar Felipe Fuentes Perea <efuentesp@gmail.com>
2019-09-06 09:27:23 -0500

    Babelify ok

--- src/views/pages/rxjs.js
+++ src/views/pages/rxjs.js
@@ -1,16 +1,17 @@
-// const rest_findAll$ = (resource, params) => {
-//   const api_params = $.param(params);
-//   const url = api_params
-//     ? `${REST_URL}/${resource}?${api_params}`
-//     : `${REST_URL}/${resource}`;
-//   console.log(url);
-
-//   return $.ajaxAsObservable({
-//     url,
-//     contentType: "application/json",
-//     dataType: "json"
-//   });
-// };
-
-// rest_findAll$("contratos", {}).subscribe(console.log);
 console.log("rxjs!");
+
+const rest_findAll$ = (resource, params) => {
+  const api_params = $.param(params);
+  const url = api_params
+    ? `${REST_URL}/${resource}?${api_params}`
+    : `${REST_URL}/${resource}`;
+  console.log(url);
+
+  return $.ajaxAsObservable({
+    url,
+    contentType: "application/json",
+    dataType: "json"
+  });
+};
+
+rest_findAll$("contratos", {}).subscribe(console.log);
