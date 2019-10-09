6641d2ade79c77d816dcfeca23e5b9cdb77e6883
efuentes <efuentes@softtek.com>
2019-10-09 17:02:59 -0500

    Se agrega primera pantalla del proyecto de 'Otros Mercados'

---
+++ src/views/pages/accimarket/100-configuracion-parametros/scripts.ts
@@ -0,0 +1,56 @@
+/// <reference path="../../typings/index.d.ts" />
+($("#linea-negocio") as any).select2({
+  placeholder: "--Seleccione--",
+  minimumResultsForSearch: Infinity
+});
+
+($("#grupo-instrumento") as any).select2({
+  placeholder: "--Seleccione--",
+  minimumResultsForSearch: Infinity
+});
+
+// Form validations
+let spreads_params: UrlParams = {};
+
+// const r$ = http_findAll$("contratos", contratos_params)
+//   .map(v => v["data"])
+//   .subscribe(data => llenaGridContratos(data));
+
+http_findAll("spreads", spreads_params, payload => {
+  llenaGridSpreads(payload);
+});
+
+const llenaGridSpreads = (spreads: any) => {
+  console.log(spreads);
+  let lastsel2;
+  $("#table_spreads").jqGrid({
+    data: spreads,
+    datatype: "local",
+    height: "auto",
+    rowList: [10, 20, 30],
+    colNames: ["Rango (Min-Max)", "Tasa Banda", "Spread", "Tasa Cliente"],
+    colModel: [
+      {
+        name: "rango-min-mx",
+        index: "rango-min-mx",
+        width: 200
+      },
+      { name: "tasa-banda", index: "emisora", width: 100, sortable: false },
+      {
+        name: "spread",
+        index: "cantidad",
+        width: 100,
+        sortable: false,
+        editable: true
+      },
+      { name: "tasa-cliente", index: "emision", width: 100, sortable: false }
+    ],
+    onSelectRow: id => {
+      if (id && id !== lastsel2) {
+        $("#table_spreads").jqGrid("restoreRow", lastsel2);
+        $("#table_spreads").jqGrid("editRow", id, true);
+        lastsel2 = id;
+      }
+    }
+  });
+};
