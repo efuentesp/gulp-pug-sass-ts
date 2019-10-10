6a73efdaa5cb9d9b9396e5136313132f3885afb3
efuentes <efuentes@softtek.com>
2019-10-10 12:02:49 -0500

    Se terminan últimos detalles de maquetado de pantalla 100

--- src/views/pages/accimarket/100-configuracion-parametros/scripts.ts
+++ src/views/pages/accimarket/100-configuracion-parametros/scripts.ts
@@ -5,9 +5,11 @@
 });
 
 ($("#grupo-instrumento") as any).select2({
-  placeholder: "--Seleccione--",
   minimumResultsForSearch: Infinity
 });
+
+$("#grupo-instrumento").val("PAGE");
+$("#grupo-instrumento").trigger("change");
 
 // Form validations
 let spreads_params: UrlParams = {};
@@ -21,8 +23,8 @@ http_findAll("spreads", spreads_params, payload => {
 });
 
 const llenaGridSpreads = (spreads: any) => {
-  console.log(spreads);
   let lastsel2;
+
   $("#table_spreads").jqGrid({
     data: spreads,
     datatype: "local",
@@ -54,3 +56,57 @@ const llenaGridSpreads = (spreads: any) => {
     }
   });
 };
+
+$("#btn_guardar_datos_inversion").click(e => {
+  console.log("dialogo_guardar_datos_inversion");
+  e.preventDefault();
+  $("#dialogo_guardar_datos_inversion").dialog({
+    modal: true,
+    closeText: "",
+    show: true,
+    title: "Guardar Datos Inversión",
+    buttons: [
+      {
+        text: "Aceptar",
+        icon: "ui-icon-check",
+        click: function() {
+          $(this).dialog("close");
+        }
+      },
+      {
+        text: "Salir",
+        icon: "ui-icon-cancel",
+        click: function() {
+          $(this).dialog("close");
+        }
+      }
+    ]
+  });
+});
+
+$("#btn_guardar_datos_spreads").click(e => {
+  console.log("dialogo_guardar_datos_spreads");
+  e.preventDefault();
+  $("#dialogo_guardar_datos_spreads").dialog({
+    modal: true,
+    closeText: "",
+    show: true,
+    title: "Guardar Datos Spreads",
+    buttons: [
+      {
+        text: "Aceptar",
+        icon: "ui-icon-check",
+        click: function() {
+          $(this).dialog("close");
+        }
+      },
+      {
+        text: "Salir",
+        icon: "ui-icon-cancel",
+        click: function() {
+          $(this).dialog("close");
+        }
+      }
+    ]
+  });
+});
