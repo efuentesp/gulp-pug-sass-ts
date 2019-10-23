19582b5301e516ef4a5422219c262912c7e997b7
efuentes <efuentes@softtek.com>
2019-10-23 14:45:49 -0500

    Se soluciona problema con parametro faltante en función fieldPlusMinus en pantalla de Solicitud de vencimientos anticipos.

--- src/views/pages/accimarket/solicitud-vencimientos-anticipos/scripts.ts
+++ src/views/pages/accimarket/solicitud-vencimientos-anticipos/scripts.ts
@@ -1,56 +1,55 @@
-fieldPlusMinus("contrato");
-fieldPlusMinus("digito");
+fieldPlusMinus("contrato", {});
+fieldPlusMinus("digito", {});
 
 ($("#libro") as any).select2({
-    minimumResultsForSearch: Infinity
+  minimumResultsForSearch: Infinity
 });
 
-
 $("#table_solicitudesVenc").jqGrid({
-    datatype: "local",
-    height: 250,
-    colNames: [
-        "Folio Operacion",
-        "Sol",
-        "Contrato",
-        "Digito",
-        "Cliente",
-        "Monto Invertido",
-        "Monto Yenc Bruto",
-        "Monto Venc Monto",
-        "Saldo Actual",
-        "Plazo",
-        "NumEmision",
-        "Consecutivo",
-        "Emision",
-        "Tasa Asig",
-        "Tasa Castigada",
-        "Fec. Venc. Aut.",
-        "Fec. Compra"
-    ],
-    colModel: [
-        { name: "folioOperacion", index: 'folioOperacion', width: 100 },
-        { name: "sol", width: 50 },
-        { name: "contrato", width: 100 },
-        { name: "digito", width: 100 },
-        { name: "cliente", width: 200 },
-        { name: "montoInv", width: 200 },
-        { name: "montoYenc", width: 200 },
-        { name: "montoVenc", width: 200 },
-        { name: "saldoActual", width: 200 },
-        { name: "plazo", width: 50 },
-        { name: "numEmision", width: 50 },
-        { name: "consecutivo", width: 50 },
-        { name: "emision", width: 150 },
-        { name: "tasaAsig", width: 80 },
-        { name: "tasaCastigada", width: 80 },
-        { name: "fechVenc", width: 80 },
-        { name: "fechCompra", width: 80 }
-    ],
-    viewrecords: true,
-    grouping: true,
-    groupingView: {
-        groupField: ['contrato'],
-        groupOrder: ['desc']
-    }
+  datatype: "local",
+  height: 250,
+  colNames: [
+    "Folio Operacion",
+    "Sol",
+    "Contrato",
+    "Digito",
+    "Cliente",
+    "Monto Invertido",
+    "Monto Yenc Bruto",
+    "Monto Venc Monto",
+    "Saldo Actual",
+    "Plazo",
+    "NumEmision",
+    "Consecutivo",
+    "Emision",
+    "Tasa Asig",
+    "Tasa Castigada",
+    "Fec. Venc. Aut.",
+    "Fec. Compra"
+  ],
+  colModel: [
+    { name: "folioOperacion", index: "folioOperacion", width: 100 },
+    { name: "sol", width: 50 },
+    { name: "contrato", width: 100 },
+    { name: "digito", width: 100 },
+    { name: "cliente", width: 200 },
+    { name: "montoInv", width: 200 },
+    { name: "montoYenc", width: 200 },
+    { name: "montoVenc", width: 200 },
+    { name: "saldoActual", width: 200 },
+    { name: "plazo", width: 50 },
+    { name: "numEmision", width: 50 },
+    { name: "consecutivo", width: 50 },
+    { name: "emision", width: 150 },
+    { name: "tasaAsig", width: 80 },
+    { name: "tasaCastigada", width: 80 },
+    { name: "fechVenc", width: 80 },
+    { name: "fechCompra", width: 80 }
+  ],
+  viewrecords: true,
+  grouping: true,
+  groupingView: {
+    groupField: ["contrato"],
+    groupOrder: ["desc"]
+  }
 });
