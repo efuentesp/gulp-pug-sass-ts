70cfed832e3cb571e0cf2c7d024cbe1158e405b7
Normaysel <alexydiego@gmail.com>
2019-10-24 18:36:24 -0500

    Se agrega el componente de listas dependientes drag and drop

--- src/views/pages/accimarket/listado/scripts.ts
+++ src/views/pages/accimarket/listado/scripts.ts
@@ -3,27 +3,31 @@
 let source_params: UrlParams = {};
 let destination_params: UrlParams = {};
 
-$("#source, #destination").listswap({
-  truncate: true,
-  height: 250,
-  is_scroll: true,
-  id_source: "source",
-  id_destination: "destination",
-  id_listSwap: "listboxswap_swap"
-});
-
 http_findAll("source", source_params, payload => {
-  llenaSource(payload);
+  llenaSource("listado", payload);
 });
 
 http_findAll("destination", destination_params, payload => {
-  llenaDestination(payload);
+  llenaDestination("listado", payload);
 });
 
-const llenaSource = (source: any) => {
-  fillSwapList("source", "listboxswap_swap", source);
+const llenaSource = (id: string, source: any) => {
+  fillSwapList(id, "source", source);
 };
 
-const llenaDestination = (destination: any) => {
-  fillSwapList("destination", "listboxswap_swap", destination);
+const llenaDestination = (id: string, destination: any) => {
+  fillSwapList(id, "destination", destination);
+};
+
+const fillSwapList = (id: string, list_id: string, params: any) => {
+  var _id = "#" + id;
+  var list = $("#listado_" + list_id);
+
+  for (var i = 0; i < params.length; i++) {
+    var data = params[i];
+    list.append("<li value = " + data.value + ">" + data.label + "</li>");
+  }
+
+  $(_id + "_source").disableSelection();
+  $(_id + "_destination").disableSelection();
 };
