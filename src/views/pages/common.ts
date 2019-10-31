124352180bb3c8f603ece45f8eafbd42762ad673
Normaysel <alexydiego@gmail.com>
2019-10-30 18:52:31 -0600

    Se ajusta autocomplete para la obtencion de valores de un servicio post

--- src/views/pages/common.ts
+++ src/views/pages/common.ts
@@ -485,6 +485,7 @@ const fieldSelectPlusAutocomplete = (id: string, params: any) => {
   const restService = params.service;
   const attrId = params.id;
   const attrText = params.text;
+  const payload = params.payload;
   let definedNodes = true;
   const numNodes = 4;
 
@@ -585,37 +586,20 @@ const fieldSelectPlusAutocomplete = (id: string, params: any) => {
       .trigger("change");
   });
 
+  var data = $.map(payload, function(item) {
+    return {
+      text: item[attrText],
+      id: item[attrId]
+    };
+  });
+
   ($(idInput) as any).select2({
-    ajax: {
-      url: `${REST_URL}/` + restService + ``,
-      dataType: "json",
-      type: "GET",
-      data: function(params) {
-        var query = {
-          q: params.term,
-          rows: 10
-        };
-        return query;
-      },
-      processResults: function(data) {
-        return {
-          results: $.map(data, function(item) {
-            return {
-              text: item[attrText],
-              id: item[attrId]
-            };
-          })
-        };
-      }
-    },
-    placeholder: {
-      id: "0",
-      text: "-- Seleccione --"
-    },
-    cache: "true",
+    data: data,
+    placeholder: "-- Seleccione --",
     minimumInputLength: 3
   });
 };
+
 const getList = (id: string) => {
   var list: any = [];
 
