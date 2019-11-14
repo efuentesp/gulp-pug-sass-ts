d799d201f32d6b5cce4b8bddcb4ec16b9a9c36c9
javoPB <pbjavouam@gmail.com>
2019-11-14 17:37:47 -0600

    Se sube codigo para spinner - loader

--- src/views/pages/accimarket/10-Test/scripts.ts
+++ src/views/pages/accimarket/10-Test/scripts.ts
@@ -9,3 +9,23 @@ $("#btn-nav-previous").click(function() {
 $("#btn-nav-next").click(function() {
   $(".menu-inner-box").animate({ scrollLeft: "+=200px" });
 });
+
+
+jQuery(function($){
+	$(document).ajaxSend(function() {
+		$("#overlay").fadeIn(300);　
+	});
+		
+	$('#button').click(function(){
+		$.ajax({
+			type: 'GET',
+			success: function(data){
+				console.log(data);
+			}
+		}).done(function() {
+			setTimeout(function(){
+				$("#overlay").fadeOut(300);
+			},500);
+		});
+	});	
+});
