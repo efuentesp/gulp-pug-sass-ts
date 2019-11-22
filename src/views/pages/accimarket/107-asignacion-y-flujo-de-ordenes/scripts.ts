2cb58360963da18adbc5d2d8e60218d6b4ffd7cb
Normaysel <alexydiego@gmail.com>
2019-11-22 12:33:44 -0600

    Revert "Se suben cambios Jesus"
    This reverts commit aabd6be5022346aa16c0ffa7556847795faac86a.

--- src/views/pages/accimarket/107-asignacion-y-flujo-de-ordenes/scripts.ts
+++ src/views/pages/accimarket/107-asignacion-y-flujo-de-ordenes/scripts.ts
@@ -532,9 +532,6 @@ const llenaGridFlujoOrdenes = (flujos: any) => {
         }
         i++;
     });
-
-    $(".table_busquedaFlujoghead_1 td:nth-child(9), .table_busquedaFlujoghead_1 td:nth-child(10)").remove();
-    $(".table_busquedaFlujoghead_0 td:nth-child(9), .table_busquedaFlujoghead_0 td:nth-child(10)").remove();
 }
 
 http_findAll("hechos", listaHechos_params, payload => {
