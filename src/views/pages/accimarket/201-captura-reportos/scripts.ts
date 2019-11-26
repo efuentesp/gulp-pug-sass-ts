f70643af6d0971a518e4f6c82a1fd162dfb19c9d
Carlos Giovanni Gonzalez Martinez <carlosg.gonzalez@softtek.com>
2019-11-26 03:54:06 -0600

    reportos-elementos

--- src/views/pages/accimarket/201-captura-reportos/scripts.ts
+++ src/views/pages/accimarket/201-captura-reportos/scripts.ts
@@ -66,7 +66,7 @@ const llenaGridCapturaReporto = (captura_reportos: any) => {
         name: "grupo",
         index: "grupo",
         align: "center",
-        width: 300,
+        width: 100,
         sortable: true
       },
       { name: "instrumento", index: "instrumento", align: "center", width: 100, sortable: true },
@@ -86,18 +86,19 @@ const llenaGridContratosReportos = (contratos_reportos: any) => {
   $("#table_gridContratos").jqGrid({
     data: contratos_reportos,
     datatype: "local",
-    toppager: true,
-    gridview: true,
-    pgtext: "P\u00E1gina {0} de {1}",
-      recordtext: "Mostrando {0} - {1} de {2}",
-      pagerpos: "center",
-    viewrecords: true,
-    autowidth: true,
-      forceFit: true,
-    height: "auto",
-      shrinkToFit: false,
-    rowNum: 20,
-      rowList: [10, 20, 30],
+    shrinkToFit: false,
+    // toppager: true,
+    // gridview: true,
+    // pgtext: "P\u00E1gina {0} de {1}",
+    //   recordtext: "Mostrando {0} - {1} de {2}",
+    //   pagerpos: "center",
+    // viewrecords: true,
+    // autowidth: true,
+    //   forceFit: true,
+    // height: "auto",
+    //   shrinkToFit: false,
+    // rowNum: 20,
+    //   rowList: [10, 20, 30],
     colNames: [
       "id_Renglon",
       "status Envio",
@@ -180,24 +181,25 @@ const llenaGridContratosReportos = (contratos_reportos: any) => {
 
 let envios_reportos_params: UrlParams = {};
 
-//GRID LLENAR ENVIOS
+//GRID LLENAR CONTRATOS
 const llenaGridEnviosReportos = (envios_reportos: any) => {
-  console.log("ENTRA A ENVIOS")
-  $("#table_gridEnvios").jqGrid({
+  console.log("ENTRA A Envios")
+  $("#table_gridEnviados").jqGrid({
     data: envios_reportos,
     datatype: "local",
-    toppager: true,
-    gridview: true,
-    pgtext: "P\u00E1gina {0} de {1}",
-      recordtext: "Mostrando {0} - {1} de {2}",
-      pagerpos: "center",
-    viewrecords: true,
-    autowidth: true,
-      forceFit: true,
-    height: "auto",
-      shrinkToFit: false,
-    rowNum: 20,
-      rowList: [10, 20, 30],
+    shrinkToFit: false,
+    // toppager: true,
+    // gridview: true,
+    // pgtext: "P\u00E1gina {0} de {1}",
+    //   recordtext: "Mostrando {0} - {1} de {2}",
+    //   pagerpos: "center",
+    // viewrecords: true,
+    // autowidth: true,
+    //   forceFit: true,
+    // height: "auto",
+    //   shrinkToFit: false,
+    // rowNum: 20,
+    //   rowList: [10, 20, 30],
     colNames: [
       "id_Renglon",
       "status Envio",
@@ -275,7 +277,106 @@ const llenaGridEnviosReportos = (envios_reportos: any) => {
       { name: "recomend", index: "recomend", align: "center", width: 50, sortable: true },
     ]
   });
-  console.log("TERMINA ENVIOS")
+  console.log("TERMINA Envios")
+};
+
+//GRID LLENAR CONTRATOS
+const llenaGridErroresReportos = (envios_reportos: any) => {
+  console.log("ENTRA A Errores")
+  $("#table_gridErrores").jqGrid({
+    data: envios_reportos,
+    datatype: "local",
+    shrinkToFit: false,
+    // toppager: true,
+    // gridview: true,
+    // pgtext: "P\u00E1gina {0} de {1}",
+    //   recordtext: "Mostrando {0} - {1} de {2}",
+    //   pagerpos: "center",
+    // viewrecords: true,
+    // autowidth: true,
+    //   forceFit: true,
+    // height: "auto",
+    //   shrinkToFit: false,
+    // rowNum: 20,
+    //   rowList: [10, 20, 30],
+    colNames: [
+      "id_Renglon",
+      "status Envio",
+      "contrato",
+      "nombre",
+      "digito",
+      "perfil_del_Contrato",
+      "saldo_Disponible",
+      "fecha_Banda",
+      "grupo",
+      "instrumento",
+      "fecha_Vencimiento",
+      "monto_a_Invertir",
+      "tasa_Pactada",
+      "plazo_Operacion",
+      "servicio_de_Inversion",
+      "tasa_Maxima",
+      "spread",
+      "prioridad",
+      "medio_de_Liquidacion_Inicio(Cargos)",
+      "medio_de_Liquidacion_Inicio(Abonos)",
+      "art.194",
+      "fechaLiq",
+      "idClasif",
+      "idDescParamTipoTasa",
+      "idUsrGpo",
+      "isGpoUser",
+      "plazo_Ini",
+      "plazo_Fin",
+      "saldo_Banda",
+      "arrayServicioInversion",
+      "idBanda",
+      "descripCortaLinea",
+      "recomend"
+    ],
+    colModel: [
+      {
+        name: "grupo",
+        index: "grupo",
+        align: "center",
+        width: 100,
+        sortable: true
+      },
+      { name: "id_Renglon", index: "id_Renglon", align: "center", width: 100, sortable: true },
+      { name: "status_Envio", index: "status Envio", align: "center", width: 100, sortable: true },
+      { name: "contrato", index: "contrato", align: "center", width: 50, sortable: true },
+      { name: "nombre", index: "nombre", align: "center", width: 50, sortable: true },
+      { name: "Digito", index: "Digito", align: "center", width: 50, sortable: true },
+      { name: "Perfil_Contrato", index: "Perfil_Contrato", align: "center", width: 50, sortable: true },
+      { name: "saldo_Disponible", index: "saldo_Disponible", align: "center", width: 50, sortable: true },
+      { name: "fecha_Banda", index: "fecha_Banda", align: "center", width: 50, sortable: true },
+      { name: "grupo", index: "grupo", align: "center", width: 50, sortable: true },
+      { name: "instrumento", index: "instrumento", align: "center", width: 50, sortable: true },
+      { name: "fecha_Vencimiento", index: "fecha_Vencimiento", align: "center", width: 50, sortable: true },
+      { name: "monto_a_Invertir", index: "monto_a_Invertir", align: "center", width: 50, sortable: true },
+      { name: "tasa_Pactada", index: "tasa_Pactada", align: "center", width: 50, sortable: true },
+      { name: "plazo_Operacion", index: "plazo_Operacion", align: "center", width: 50, sortable: true },
+      { name: "servicio_de_Inversion", index: "servicio_de_Inversion", align: "center", width: 50, sortable: true },
+      { name: "tasa_Maxima", index: "tasa_Maxima", align: "center", width: 50, sortable: true },
+      { name: "spread", index: "spread", align: "center", width: 50, sortable: true },
+      { name: "prioridad", index: "prioridad", align: "center", width: 50, sortable: true },
+      { name: "liquidacion(Cargos)", index: "liquidacion(Cargos)", align: "center", width: 50, sortable: true },
+      { name: "liquidacion(Abonos)", index: "liquidacion(Abonos)", align: "center", width: 50, sortable: true },
+      { name: "art.194", index: "art.194", align: "center", width: 50, sortable: true },
+      { name: "fechaLiq", index: "fechaLiq", align: "center", width: 50, sortable: true },
+      { name: "idClasif", index: "idClasif", align: "center", width: 50, sortable: true },
+      { name: "idDescParamTipoTasa", index: "idDescParamTipoTasa", align: "center", width: 50, sortable: true },
+      { name: "idUsrGpo", index: "idUsrGpo", align: "center", width: 50, sortable: true },
+      { name: "plazo_Ini", index: "plazo_Ini", align: "center", width: 50, sortable: true },
+      { name: "plazo_Fin", index: "plazo_Fin", align: "center", width: 50, sortable: true },
+      { name: "saldo_Banda", index: "saldo_Banda", align: "center", width: 50, sortable: true },
+      { name: "arrayServicioInversion", index: "arrayServicioInversion", align: "center", width: 50, sortable: true },
+      { name: "idBanda", index: "idBanda", align: "center", width: 50, sortable: true },
+      { name: "descripCortaLinea", index: "descripCortaLinea", align: "center", width: 50, sortable: true },
+      { name: "recomend", index: "recomend", align: "center", width: 50, sortable: true },
+    ]
+  });
+  console.log("TERMINA Errores")
 };
 
 // NOTA: REESCALADO DE GRID CAPTURA
@@ -350,4 +451,5 @@ $(".sidebar_button").on("click", function() {
 llenaGridCapturaReporto([]);
 llenaGridContratosReportos([]);
 llenaGridEnviosReportos([]);
+llenaGridErroresReportos([]);
 
