eb7b91b8e7889d726088377ecb209b264046383a
Normaysel <alexydiego@gmail.com>
2019-12-26 18:21:54 -0600

    Se elimina ayuda, a petición de usuario Carlos Camacho en correo del día 24/12/2019

--- src/views/pages/common.ts
+++ src/views/pages/common.ts
@@ -771,8 +771,7 @@ const fieldSelectPlusAutocomplete = (id: string, params: any) => {
   ($(idInput) as any).select2({
     language: "es",
     data: data,
-    placeholder: "",
-    minimumInputLength: 1
+    placeholder: ""
   });
 };
 
